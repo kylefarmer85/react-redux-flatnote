986c666147c195f8e51f3eba15da3b9039c42bfb
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-07 00:23:45 -0600

    random sticky note images working

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,24 +2,34 @@ import React from 'react';
 import { useHistory } from 'react-router'
 import { Card, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import blue from '../images/blue.png'
+import orange from '../images/orange.jpeg'
+import sticky from '../images/sticky.png'
+import pink from '../images/pink.png'
+
+
+
+const randomNoteColor = () => {
+  const noteImages = [blue, orange, sticky, pink]
+  return noteImages[Math.floor(Math.random() * noteImages.length)]
+}
+
 
 const Note = (props) => {
   const {id, title, content} = props
-
   const history = useHistory()
 
   return (
 
-    <Card>
-      <Card.Content header={title} />
-      <Card.Content description={content} />
-      <Card.Content extra>
-        <div className='ui two buttons'>
+      <div className="color-note" style={{backgroundImage:`url(${randomNoteColor()})`}} >
+        <div className="inner-note">
+          <h3>{title}</h3>
+          <p>{content}</p>
           <Button onClick={() => history.push(`/notes/${id}`) }>View</Button>
           <Button primary as={Link} to={`/notes/${id}/edit`}>Edit</Button>
         </div>
-      </Card.Content>
-    </Card>
+    </div>
+   
 
   );
 }
--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,31 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 import Note from './Note'
-import { Card } from 'semantic-ui-react'
 
 const NotesContainer = (props) => {
 
-  const [notes, setNotes] = useState([])
- 
-  useEffect(() => {
-   if (props.notes.loading === false) {
-     setNotes(props.notes.notes) 
-   }
-  },[props.notes])
-
   return (
     <div>
-      <h1 className="notes-header">{ props.notes.loading ? "please login"  : `${props.user.username}'s notes` }</h1>
-      <Card.Group className="notes-container">
-        { props.notes.loading ? null : notes.map(note => {
+      <h1 className="notes-header">{ props.notes.loading ? "Loading..."  : `${props.user.username}'s notes` }</h1>
+
+      <div className="notes-container">
+        { props.notes.loading ? null : props.notes.notes.map(note => {
           return <Note {...note} key={note.id} />
         }) 
         }
-      </Card.Group>
+      </div>
     </div>
   );
 } 
-
 
 const mapStateToProps = (state) => {
   return {
