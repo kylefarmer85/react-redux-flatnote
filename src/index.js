5fabe99815ca0069c3f4f0e73191253c2799af04
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-08 18:18:20 -0600

    changed the way thunk and redux devtools were accesed, imported compose from redux

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/index'
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import App from './App';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import history from './history'
 
-const middleware = [
-  thunk
-]
 
-let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunk)));
 
 
 ReactDOM.render(
